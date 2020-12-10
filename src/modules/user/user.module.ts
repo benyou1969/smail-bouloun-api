744dd53e8628ad51f9bc6f73b95cd6ad320dbdcb
benyou1969 <benyou.11@hotmail.com>
2020-12-10 17:40:15 +0100

    Work In Progress
    This is a trash commit to save changes

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,30 +1,13 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtStrategy } from '../auth/strategies/jwt-auth.strategy';
 import { UserController } from './controller/user.controller';
 import { UserService } from './service/user.service';
 import { UserRepository } from './user.repository';
 
 @Module({
   controllers: [UserController],
-  imports: [
-    PassportModule,
-    JwtModule.register({
-      secret: 'TopSecret51',
-      signOptions: {
-        expiresIn: 3600,
-      },
-    }),
-    TypeOrmModule.forFeature([UserRepository]),
-  ],
-  providers: [JwtStrategy, UserService],
-  exports: [
-    TypeOrmModule.forFeature([UserRepository]),
-    JwtStrategy,
-    PassportModule,
-    UserService,
-  ],
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  providers: [UserService],
+  exports: [TypeOrmModule.forFeature([UserRepository]), UserService],
 })
 export class UserModule {}
